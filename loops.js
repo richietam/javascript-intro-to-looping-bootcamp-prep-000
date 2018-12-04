e069b9ba5538e8c44ab56e0687a85af4f9edf311
richardtam.ny <richardtam.ny@gmail.com>
2018-12-04 19:25:01 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop(array) {
+  var array = '';
+  for(var i=0; i<25;i++);
+    if (i===1){
+    array += "I am 1 strange loop"
+  } else {
+    array += "I am ${i} strange loops"
+  }
+  return array
+}
+
+function whileLoop(n) {
+  n = 25
+  while (n > 0) {
+    console.log("--n")
+  }
+  }
